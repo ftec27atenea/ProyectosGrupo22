a690d81d33ebbacde9d0463fecce78948616b05e
Nicolas Fiquitiva Segura <Nico52105@gmail.com>
2023-07-24 20:21:27 -0500

    se sube trabajo en clase

--- app/src/main/java/com/example/proyectosgrupo22/MainActivity.java
+++ app/src/main/java/com/example/proyectosgrupo22/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.proyectosgrupo22;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sesion_32);
+        TextView texto=findViewById(R.id.mi_texto);
+        Animation animacion= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.animacion_01);
+        texto.startAnimation(animacion);
     }
 }
