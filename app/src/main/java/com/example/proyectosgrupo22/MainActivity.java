54988eb87f626887231e762284629b12a2366fad
JohnJa78 <johnspyro37@gmail.com>
2023-07-25 20:01:09 -0500

    Trabajo en clase

--- app/src/main/java/com/example/proyectosgrupo22/MainActivity.java
+++ app/src/main/java/com/example/proyectosgrupo22/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.proyectosgrupo22;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sesion_32);
+        TextView textView=findViewById(R.id.spyro);
+        Animation animation= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.animacion_01);
+        textView.startAnimation(animation);
+
     }
 }
