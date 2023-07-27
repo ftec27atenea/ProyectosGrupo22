ed60425b93d60bb14044b828b44733c6d17027b7
JohnJa78 <johnspyro37@gmail.com>
2023-07-26 20:16:22 -0500

    trabajo en clase

---
+++ app/src/main/java/com/example/proyectosgrupo22/sesion_34.java
@@ -0,0 +1,35 @@
+package com.example.proyectosgrupo22;
+
+import static com.example.proyectosgrupo22.R.id.tvEdad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class sesion_34 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sesion34);
+
+        int edad = 45;
+        double altura = 1.70;
+        boolean estudiante = true;
+        String nombre = "Jairo Ortiz";
+
+        TextView tvEdad=findViewById(R.id.tvEdad);
+        TextView tvAltura=findViewById(R.id.tvAltura);
+        TextView tvEstudiante=findViewById(R.id.tvEstudiante);
+        TextView tvNombre=findViewById(R.id.tvNombre);
+
+
+        tvEdad.setText(String.valueOf(edad));
+        tvAltura.setText(String.valueOf(altura));
+        tvEstudiante.setText(String.valueOf(estudiante));
+        tvNombre.setText(nombre);
+
+    }
+}
