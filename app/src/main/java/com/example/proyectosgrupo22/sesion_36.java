6ab40a1ff41594d807dc3e46fa8eb1f5dc297cbc
JohnJa78 <johnspyro37@gmail.com>
2023-08-01 20:09:44 -0500

    Update sesion_36.java

--- app/src/main/java/com/example/proyectosgrupo22/sesion_36.java
+++ app/src/main/java/com/example/proyectosgrupo22/sesion_36.java
@@ -3,6 +3,7 @@ package com.example.proyectosgrupo22;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class sesion_36 extends AppCompatActivity {
 
@@ -10,5 +11,17 @@ public class sesion_36 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sesion36);
+    }
+
+    public void  ObtenerFormaDeTransporte(View view){
+        CheckBox OpcionCarro=findViewById(R.id.OpcionCarro);
+        CheckBox OpcionMoto=findViewById(R.id.OpcionCarro);
+        CheckBox OpcionBicicleta=findViewById(R.id.OpcionCarro);
+
+        String carro="Carro:"+OpcionCarro.isChecked();
+        String moto="Moto:"+OpcionMoto.isChecked();
+        String bicicleta="Bicicleta:"+OpcionBicicleta.isChecked();
+
+
     }
 }
