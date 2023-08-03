c1c2539aad946abb4c5493e7ff325cd690016604
K3v1nG <guzmanpovedakevin@gmail.com>
2023-08-02 19:58:21 -0500

    kvin

---
+++ app/src/main/java/com/example/proyectosgrupo22/sesion_36.java
@@ -0,0 +1,21 @@
+package com.example.proyectosgrupo22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaTimestamp;
+import android.os.Bundle;
+import android.view.View;
+
+public class sesion_36 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sesion_36);
+
+
+    }
+    public void MostrarElTexto(View view){
+
+    }
+}
