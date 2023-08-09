1019e4fbdb89fb805529ef5c9b78917ed442037a
K3v1nG <guzmanpovedakevin@gmail.com>
2023-08-08 19:30:40 -0500

    kevin

---
+++ app/src/main/java/com/example/proyectosgrupo22/sesion08agos.java
@@ -0,0 +1,14 @@
+package com.example.proyectosgrupo22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sesion08agos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sesion08agos);
+    }
+}
