d6265fab0fa70be19b5cae2b26afeb7ec336a8b3
smithramirezc <137573577+smithramirezc@users.noreply.github.com>
2023-08-15 23:25:32 -0500

    Trabajo en clase

---
+++ app/src/main/java/com/example/proyectosgrupo22/sesion_38.java
@@ -0,0 +1,14 @@
+package com.example.proyectosgrupo22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sesion_38 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sesion_38);
+    }
+}
