92aadfc0c5781bbba619d82d30eb04bd94354586
guakero <116929201+guakero@users.noreply.github.com>
2023-08-22 20:07:27 -0500

    sesion42

---
+++ app/src/main/java/com/example/proyectosgrupo22/session42.java
@@ -0,0 +1,18 @@
+package com.example.proyectosgrupo22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class session42 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.session42);
+
+
+    }
+}
